9b7dc987eea8da268abe4b295e2b1cc46d5bbcef
Facu <famatas@ucasal.edu.ar>
2020-01-27 09:44:07 -0300

    avances integracion api

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../../services/api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: []
+})
+export class HomeComponent implements OnInit {
+
+  constructor( private api: ApiService ) {  
+       this.api.predictExample(); 
+  }
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  baseUrl = 'http://thesis-backend-sa.herokuapp.com/';
+  postData = {
+    example: 'Mala idea',
+    sentiment: 0
+  };
+
+  constructor(private http: HttpClient) { }
+
+  predictExample(){
+    return this.http.post(this.baseUrl, this.postData)
+    .toPromise().then(data => {
+      console.log(data);
+    });
+
+  }
+}
